edd3c764f3303a95f64145308b495a8a0218e6bd
Andrii Danchenko <danchenko.andrii@pdffiller.team>
2022-06-05 21:06:14 +0300

    env prepared

---
+++ src/pacman.ts
@@ -0,0 +1,3 @@
+import { HELLO_TEXT } from './constants';
+
+console.log(HELLO_TEXT + '!');
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+const config = {
+  entry: './src/pacman.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.ts',
+      '.js'
+    ]
+  },
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname, 'dist'),
+        publicPath: '/',
+      },
+      {
+        directory: path.join(__dirname, 'src/css'),
+        publicPath: '/css',
+      },
+      {
+        directory: path.join(__dirname, 'img'),
+        publicPath: '/img',
+      },
+    ],
+  },
+};
+
+module.exports = config;
